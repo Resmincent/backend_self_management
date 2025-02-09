7ce28d590366a52ecbc40480d6c1213618456bdc
Ifran Hilmi Azhara <itdev.support13@tokiomarine-life.co.id>
2025-02-09 13:32:59 +0700

    add connection for self-mmanageent db

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = 'localhost';
+$username = 'root';
+$password = 'root';
+$dbname = 'db_self_management';
+
+try {
+    $dsn = "mysql:host=$host;dbname=$dbname";
+    $conn = new PDO($dsn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connection Success";
+} catch (PDOException $e) {
+    echo json_encode(array(
+        "error" => "Connection Failed: " . $e->getMessage(),
+    ));
+    $connUser = null;
+    $conn = null;
+}

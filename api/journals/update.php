96251b0409d3956ebfb248375646f6a599df0dad
Ifran Hilmi Azhara <itdev.support13@tokiomarine-life.co.id>
2025-03-28 09:58:01 +0700

    add update journals

---
+++ api/journals/update.php
@@ -0,0 +1,36 @@
+<?php
+
+require "../../connection.php";
+require "../response.php";
+
+$userId = $_POST['user_id'];
+$category = $_POST['category'];
+$title = $_POST['title'];
+$content = $_POST['content'];
+$reference = $_POST['reference'];
+$id = $_POST['id'];
+
+try {
+    $sqlUpdate = "UPDATE journals SET 
+                user_id     = '$userId',
+                category     = '$category',
+                title     = '$title',
+                content    = '$content',
+                WHERE id    = '$id'
+                ";
+
+    $conn->exec($sqlUpdate);
+    $responseBody = array(
+        "message" => "Success update journals",
+    );
+
+    sendResponse(201, $responseBody);
+} catch (PDOException $e) {
+    $responseBody =  array(
+        "message" => "Failed update journals",
+        "error" =>  $e->getMessage(),
+    );
+    sendResponse(500, $responseBody);
+} finally {
+    $conn = null;
+}

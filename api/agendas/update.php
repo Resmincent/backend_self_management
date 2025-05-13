037098a0295ed8fd3122b0784663652b58a724c2
Ifran Hilmi Azhara <itdev.support13@tokiomarine-life.co.id>
2025-05-13 09:32:14 +0700

    update api agenda

--- api/agendas/update.php
+++ api/agendas/update.php
@@ -5,14 +5,20 @@ require "../response.php";
 
 $userId = $_POST['user_id'];
 $id = $_POST['id'];
+$title = $_POST['title'];
+$category = $_POST['category'];
 $startEvent = $_POST['start_event'];
 $endEvent = $_POST['end_event'];
+$description = $_POST['description'];
 
 try {
     $sqlUpdate = "UPDATE agendas SET 
                 user_id     = '$userId',
+                title     = '$title',
+                category     = '$category',
                 start_event  = '$startEvent',
-                end_event    = '$endEvent'
+                end_event    = '$endEvent',
+                description  = '$description'
                 WHERE id    = '$id'
                 ";
 
